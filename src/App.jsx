2ad61757b62bf80042df62920349720648626dd3
sahilyadav-15 <s6906433@gmail.com>
2025-05-23 13:31:35 +0530

    Final change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1000,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const validInput = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {validInput ? <Results input={userInput} /> : <p className="center">Please enter a duration greater than 0</p>}
+    </>
+  );
 }
 
-export default App
+export default App;
